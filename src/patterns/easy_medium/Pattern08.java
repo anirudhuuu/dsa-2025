7d7055c3e07f6a5cbc129b8c7644cb5e9c7874e2
Anirudh Jwala <anirudhsai2@gmail.com>
2025-03-23 12:50:15 +0530

    pattern problems

---
+++ src/patterns/easy_medium/Pattern08.java
@@ -0,0 +1,30 @@
+package patterns.easy_medium;
+
+/**
+ * Given an integer n. You need to recreate the pattern
+ * given below for any value of N. Let's say for N = 5,
+ * the pattern should look like as below:
+ * <p>
+ * <p>*********</p>
+ * <p> ******* </p>
+ * <p>  *****  </p>
+ * <p>   ***   </p>
+ * <p>    *    </p>
+ * Print the pattern in the function given to you.
+ */
+public class Pattern08 {
+    void pattern08(int n) {
+        for (int i = n - 1; i >= 0; i--) {
+            // Spaces
+            for (int j = 0; j < (n - i - 1); j++) {
+                System.out.print(" ");
+            }
+
+            // Stars
+            for (int j = 0; j < ((2 * i) + 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
