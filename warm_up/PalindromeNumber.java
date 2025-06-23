3361cf1cb894d1fb1652920a6053f9fc8e553508
Anirudh Jwala <anirudhsai2@gmail.com>
2025-06-24 00:11:20 +0530

    feat: implement PalindromeNumber class to check if an integer is a palindrome

---
+++ warm_up/PalindromeNumber.java
@@ -0,0 +1,25 @@
+package warm_up;
+
+public class PalindromeNumber {
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int temp = x;
+        int reversed = 0;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            reversed = reversed * 10 + digit;
+            temp = temp / 10;
+        }
+
+        return reversed == x;
+    }
+
+    public static void main(String[] args) {
+        int number = -121;
+        System.out.println(isPalindrome(number));
+    }
+}
