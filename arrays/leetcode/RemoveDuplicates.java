990bd1bb64dfbda0cffa6ba308c0d8d14d8e7a41
Anirudh Jwala <anirudhsai2@gmail.com>
2025-06-24 01:08:59 +0530

    feat: add method to remove duplicates from an array and print unique elements

--- arrays/leetcode/RemoveDuplicates.java
+++ arrays/leetcode/RemoveDuplicates.java
@@ -44,4 +44,28 @@ class RemoveDuplicates {
 
         return k;
     }
+
+    public static int removeDuplicatesArr(int[] arr) {
+        int x = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[x] && arr[i] != arr[x]) {
+                x = x + 1;
+                arr[x] = arr[i];
+            }
+        }
+
+        for (int i = 0; i < x + 1; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        return x + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+        int k = removeDuplicatesArr(arr);
+        System.out.println(k);
+    }
 }
