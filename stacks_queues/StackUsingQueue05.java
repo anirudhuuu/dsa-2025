2e41e1df97cb67f4a8e9600712cd7761e6bd7de2
anirudhsai2 <anirudhsai2@gmail.com>
2025-08-29 18:25:43 +0530

    feat: implement StackUsingQueue class to support stack operations using a single queue, including push, pop, top, and isEmpty

---
+++ stacks_queues/StackUsingQueue05.java
@@ -0,0 +1,55 @@
+package stacks_queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Implement Stack using Queue
+ * ==============================
+ * Implement a Last-In-First-Out (LIFO) stack using a single queue. The
+ * implemented stack should support the following operations: push, pop, top,
+ * and isEmpty.
+ * 
+ * Implement the QueueStack class:
+ * - void push(int x): Pushes element x onto the stack.
+ * - int pop(): Removes and returns the top element of the stack.
+ * - int top(): Returns the top element of the stack without removing it.
+ * - boolean isEmpty(): Returns true if the stack is empty, false otherwise.
+ */
+public class StackUsingQueue05 {
+    private Queue<Integer> queue;
+
+    public StackUsingQueue05() {
+        queue = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        int size = queue.size();
+        queue.add(x);
+
+        for (int i = 0; i < size; i++) {
+            queue.add(queue.poll());
+        }
+    }
+
+    public int pop() {
+        return queue.poll();
+    }
+
+    public int top() {
+        return queue.peek();
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        StackUsingQueue05 stack = new StackUsingQueue05();
+        stack.push(4);
+        stack.push(8);
+        System.out.println(stack.pop()); // returns 8
+        System.out.println(stack.top()); // returns 4
+        System.out.println(stack.isEmpty()); // returns false
+    }
+}
