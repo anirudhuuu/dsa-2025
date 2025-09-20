68a72df4dcd236aa2468727bf1e713eec5c68acd
anirudhsai2 <anirudhsai2@gmail.com>
2025-09-20 22:00:42 +0530

    feat: replace LargestElement class with LargestElement02 and add LinearSearch01 class for array operations

---
+++ arrays/fundamentals/LargestElement02.java
@@ -0,0 +1,50 @@
+package arrays.fundamentals;
+
+import java.util.Arrays;
+
+/**
+ * Largest Element
+ * ==================
+ * Given an array of integers nums, return the
+ * value of the largest element in the array
+ */
+public class LargestElement02 {
+    /**
+     * Time Complexity: O(n log n)
+     * Space Complexity: O(1)
+     */
+    public static int largestElementBrute(int[] nums) {
+        // When the input is empty
+        if (nums.length == 0)
+            return -1;
+
+        // Sort the given array in ascending ordering
+        Arrays.sort(nums);
+
+        return nums[nums.length - 1];
+    }
+
+    /**
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     */
+    public static int largestElementBest(int[] nums) {
+        if (nums.length == 0)
+            return -1;
+
+        int largestElement = nums[0];
+
+        for (int num : nums) {
+            largestElement = Math.max(largestElement, num);
+        }
+
+        return largestElement;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { -4, -3, 0, 1, -8 };
+
+        System.out.println(largestElementBrute(arr));
+        System.out.println(largestElementBest(arr));
+    }
+}
--- arrays/fundamentals/LinearSearch01.java
+++ arrays/fundamentals/LinearSearch01.java
@@ -1,12 +1,17 @@
 package arrays.fundamentals;
 
 /**
+ * Linear Search
+ * ===============
  * Given an array of integers nums and an integer target,
  * find the smallest index (0 based indexing) where the target
  * appears in the array. If the target is not found in the array, return -1
+ * 
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
  */
-public class LinearSearch {
-    int linearSearch(int[] nums, int target) {
+public class LinearSearch01 {
+    public static int linearSearch(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == target) {
                 return i;
@@ -14,5 +19,12 @@ public class LinearSearch {
         }
 
         return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, -4, 4, 0, 10 };
+        int key = 6;
+
+        System.out.println(linearSearch(arr, key));
     }
 }
