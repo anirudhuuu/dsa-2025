48523972c36b4db327997b81d439ba16b868400c
anirudhsai2 <anirudhsai2@gmail.com>
2025-09-20 21:52:49 +0530

    feat: add ReverseArray04 class to reverse an array in-place with O(n) time complexity

--- arrays/basics/ReverseArray04.java
+++ arrays/basics/ReverseArray04.java
@@ -1,15 +1,17 @@
 package arrays.basics;
 
 /**
+ * Reverse an array
+ * ==================
  * Given an array arr of n elements. The task is to reverse
  * the given array. The reversal of array should be in-place.
  */
-public class ReverseArray {
+public class ReverseArray04 {
     /**
      * Time Complexity: O(n)
      * Space Complexity: O(1)
      */
-    void reverse(int[] arr, int n) {
+    public static void reverse(int[] arr, int n) {
         int low = 0;
         int high = n - 1;
 
@@ -21,5 +23,15 @@ public class ReverseArray {
             low += 1;
             high -= 1;
         }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 1, 1, 5, 1 };
+        reverse(arr, arr.length);
+
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
     }
 }
