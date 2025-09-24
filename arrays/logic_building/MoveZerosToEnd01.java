d6f209dae005bfc5d1b6686c371622b33f0e4547
anirudhsai2 <anirudhsai2@gmail.com>
2025-09-25 00:49:12 +0530

    feat: add MoveZerosToEnd01 class to move all zeros in an array to the end while maintaining the order of non-zero elements

--- arrays/logic_building/MoveZerosToEnd01.java
+++ arrays/logic_building/MoveZerosToEnd01.java
@@ -1,14 +1,21 @@
 package arrays.logic_building;
 
 /**
+ * Move Zeros to End
+ * ==================
  * Given an integer array nums, move all the 0's to the end of the array.
  * The relative order of the other elements must remain the same.
- * <p>
+ * 
  * This must be done in place, without making a copy of the array.
  */
-public class MoveZerosToEnd {
-    void moveZeroes(int[] nums) {
+public class MoveZerosToEnd01 {
+    /**
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     */
+    public static void moveZeroes(int[] nums) {
         int currentIndex = 0;
+
         // Capture all non-zero values and set from start
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
@@ -21,5 +28,15 @@ public class MoveZerosToEnd {
         for (int i = currentIndex; i < nums.length; i++) {
             nums[i] = 0;
         }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 0, 1, 0, 3, 12 };
+        moveZeroes(nums);
+
+        for (int val : nums) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
     }
 }
