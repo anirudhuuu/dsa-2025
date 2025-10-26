5283afcb6485fe2b99efe8f3bb71a59043dd510d
anirudhsai2 <anirudhsai2@gmail.com>
2025-10-27 01:07:39 +0530

    feat: add KthLargest class to find the k-th largest element in an array

---
+++ heaps/KthLargest11.java
@@ -0,0 +1,40 @@
+package heaps;
+
+import java.util.PriorityQueue;
+
+/**
+ * K-th Largest element in an array
+ * =================================
+ * Given an array nums, return the k-th largest element in the array.
+ * 
+ * Time Complexity: O(N * log K)
+ * Space Complexity: O(K)
+ */
+public class KthLargest11 {
+    public static int kthLargestElement(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        // add the first k-elements in min-heap
+        for (int i = 0; i < k; i++) {
+            pq.add(nums[i]);
+        }
+
+        // process the rest of the (N - k) elements
+        for (int i = k; i < nums.length; i++) {
+            if (nums[i] > pq.peek()) {
+                // remove the smallest from min-heap
+                pq.poll();
+
+                // add the current element into the min-heap
+                pq.add(nums[i]);
+            }
+        }
+
+        return pq.peek();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { -5, 4, 1, 2, -3 };
+        System.out.println("K-th largest :: " + kthLargestElement(nums, 5));
+    }
+}
